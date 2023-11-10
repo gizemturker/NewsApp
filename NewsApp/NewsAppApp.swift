d122678eefa4360a88092620ac6a48cb9281d858
Gizem Turker <gzm.aiesec@gmail.com>
2023-11-10 18:18:01 +0300

    initial commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Gizem Turker on 10.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
